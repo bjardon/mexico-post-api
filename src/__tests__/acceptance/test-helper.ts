2e7e388c2faf4c1013d89ad121f34f1aeb99737d
Bruno Jardón <bjardon97@gmail.com>
2020-05-10 16:11:16 -0500

    Initialized empty Loopback 4 project

---
+++ src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {MexicoPostApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new MexicoPostApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: MexicoPostApplication;
+  client: Client;
+}
---
+++ src/repositories/index.ts
@@ -0,0 +1,6 @@
+export * from './city.repository';
+export * from './municipality.repository';
+export * from './post-code.repository';
+export * from './post-office.repository';
+export * from './state.repository';
+export * from './township.repository';
